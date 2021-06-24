ae23540c91fab0db6f798e2d7d6672dbb61be676
Jblack017 <blackhamjoshua@comcast.net>
2021-06-24 14:59:20 -0600

    working fetch

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,22 +13,21 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    @project = Project.create(name:params[:name], description:params[:description])
-    
+    @project = Project.create(name: params[:project][:name], description: params[:project][:description])
+
     if @project.valid?
       @project.save
-
       @new_graph = Graph.create(
         project_id: @project.id,
-        stock_sym: params[:graph][:stock_sym], 
-        x_axis_array: params[:graph][:x_axis_array], 
-        proj_low_array: params[:graph][:proj_low_array], 
-        proj_high_array: params[:graph][:proj_high_array], 
-        proj_array: params[:graph][:proj_array])
+        stock_sym: params[:project][:graph][:stock_sym], 
+        x_axis_array: params[:project][:graph][:x_axis_array], 
+        proj_low_array: params[:project][:graph][:proj_low_array], 
+        proj_high_array: params[:project][:graph][:proj_high_array], 
+        proj_array: params[:project][:graph][:proj_array])
 
       return render json: @project, status: :created
     else
-      return render json: {error: newProject.errors.full_messages}
+      return render json: {error: @project.errors.full_messages}
     end
   end
 
