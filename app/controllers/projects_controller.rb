af91b0d748b6f3e61228085777567eedea367939
Jblack017 <blackhamjoshua@comcast.net>
2021-06-24 14:28:21 -0600

    working project post with graph route

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,4 +12,24 @@ class ProjectsController < ApplicationController
     render json: @project, include: :graphs
   end
 
+  def create
+    @project = Project.create(name:params[:name], description:params[:description])
+    
+    if @project.valid?
+      @project.save
+
+      @new_graph = Graph.create(
+        project_id: @project.id,
+        stock_sym: params[:graph][:stock_sym], 
+        x_axis_array: params[:graph][:x_axis_array], 
+        proj_low_array: params[:graph][:proj_low_array], 
+        proj_high_array: params[:graph][:proj_high_array], 
+        proj_array: params[:graph][:proj_array])
+
+      return render json: @project, status: :created
+    else
+      return render json: {error: newProject.errors.full_messages}
+    end
+  end
+
 end
--- app/models/graph.rb
+++ app/models/graph.rb
@@ -1,3 +1,4 @@
 class Graph < ApplicationRecord
   belongs_to :project
+  
 end
